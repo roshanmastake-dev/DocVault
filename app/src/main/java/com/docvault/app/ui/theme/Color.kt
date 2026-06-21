7a6895d87b3d57c0098640262477146e520a088f
roshanmastake-dev <roshanmastake1564@gmail.com>
2026-06-21 13:00:35 +0530

    Add color definitions for app theme

---
+++ app/src/main/java/com/docvault/app/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.docvault.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundLight = Color(0xFFFAF8F5)
+val SurfaceLight = Color(0xFFFFFFFF)
+val PrimaryTeal = Color(0xFF3A8E8C)
+val PrimaryTealDark = Color(0xFF2E6E6C)
+val AccentPeach = Color(0xFFFFB199)
+val TextPrimary = Color(0xFF2B2B2B)
+val TextSecondary = Color(0xFF8A8A8A)
+val DeleteRed = Color(0xFFD96C6C)
